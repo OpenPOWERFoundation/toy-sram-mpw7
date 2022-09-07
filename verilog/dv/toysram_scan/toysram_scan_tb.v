e1f71ea34b631048c0a1f8dfd52505ebc9ef7148
openpowerwtf <52765606+openpowerwtf@users.noreply.ggithub.com>
2022-09-07 08:41:12 -0500

    start talking to i/o

--- verilog/dv/toysram_scan/toysram_scan_tb.v
+++ verilog/dv/toysram_scan/toysram_scan_tb.v
@@ -13,29 +13,25 @@
 // limitations under the License.
 // SPDX-License-Identifier: Apache-2.0
 
-/*
-   toysram site scan test
-
-   # control.v
-   test_enable = dut.io_in[0];
-   scan_clk = dut.io_in[1];
-   scan_di = dut.io_in[2];
-   scan_do = dut.io_out[3]
-
-   io_ra0_clk = dut.io_in[4];
-   io_ra0_rst = dut.io_in[5];
-   io_ra0_r0_enb = dut.io_in[6];
-   io_ra0_r1_enb = dut.io_in[7];
-   io_ra0_w0_enb = dut.io_in[8];
-
- */
-
-
 `default_nettype none
 
 `timescale 1 ns / 1 ps
 
+// Test
+`define PIN_TE 8
+// Scan
+`define PIN_SCAN_CLK 9
+`define PIN_SCAN_IN 10
+`define PIN_SCAN_OUT 11
+// RA0
+`define PIN_RA0_CLK 12
+`define PIN_RA0_RST 13
+`define PIN_RA0_R0_EN 14
+`define PIN_RA0_R1_EN 15
+`define PIN_RA0_W0_EN 16
+
 module toysram_scan_tb;
+
 	reg clock;
 	reg RSTB;
 	reg CSB;
@@ -44,13 +40,33 @@ module toysram_scan_tb;
 
 	wire gpio;
 	wire [37:0] mprj_io;
-	wire [7:0] mprj_io_0;
 
-	//assign mprj_io_0 = mprj_io[7:0];
-	// assign mprj_io_0 = {mprj_io[8:4],mprj_io[2:0]};
+   //?
+	assign mprj_io[3] = (CSB == 1'b1) ? 1'b1 : 1'bz;
 
-	//assign mprj_io[3] = (CSB == 1'b1) ? 1'b1 : 1'bz;
-	// assign mprj_io[3] = 1'b1;
+   // *********************************************************************************
+   // driver connections
+   wire debug = 1;
+   reg pin_te, pin_scan_clk, pin_scan_in, pin_scan_out;
+   reg pin_ra0_clk, pin_ra0_rst, pin_ra0_r0_en, pin_ra0_r1_en, pin_ra0_w0_en;
+
+   // drive
+   assign mprj_io[`PIN_TE] = pin_te;
+   assign mprj_io[`PIN_SCAN_CLK] = pin_scan_clk;
+   assign mprj_io[`PIN_SCAN_IN] = pin_scan_in;
+   assign mprj_io[`PIN_RA0_CLK] = pin_ra0_clk;
+   assign mprj_io[`PIN_RA0_RST] = pin_ra0_rst;
+   assign mprj_io[`PIN_RA0_R0_EN] = pin_ra0_r0_en;
+   assign mprj_io[`PIN_RA0_R1_EN] = pin_ra0_r1_en;
+   assign mprj_io[`PIN_RA0_W0_EN] = pin_ra0_w0_en;
+
+   // sample
+   always @(*) begin
+      pin_scan_out <= mprj_io[`PIN_SCAN_OUT];
+   end
+
+   // *********************************************************************************
+   // clocks
 
 	// External clock is used by default.  Make this artificially fast for the
 	// simulation.  Normally this would be a slow clock and the digital PLL
@@ -62,56 +78,47 @@ module toysram_scan_tb;
 		clock = 0;
 	end
 
+   // *********************************************************************************
+   // setup
+
 	initial begin
 		$dumpfile("toysram_scan.vcd");
 		$dumpvars(0, toysram_scan_tb);
+      $display("\n\n");
 
 		// Repeat cycles of 1000 clock edges as needed to complete testbench
 		repeat (25) begin
 			repeat (1000) @(posedge clock);
-			// $display("+1000 cycles");
+			$display("+1000 cycles");
 		end
 		$display("%c[1;31m",27);
 		`ifdef GL
-			$display ("Monitor: Timeout, Test Mega-Project IO Ports (GL) Failed");
+			$display ("*ERROR* Timeout, Test Mega-Project IO Ports (GL) Failed");
 		`else
-			$display ("Monitor: Timeout, Test Mega-Project IO Ports (RTL) Failed");
+			$display ("*ERROR* Timeout, Test Mega-Project IO Ports (RTL) Failed");
 		`endif
 		$display("%c[0m",27);
 		$finish;
 	end
 
-	initial begin
-	    // Observe Output pins [7:0]
-		wait(mprj_io_0 == 8'h01);
-		wait(mprj_io_0 == 8'h02);
-		wait(mprj_io_0 == 8'h03);
-		wait(mprj_io_0 == 8'h04);
-		wait(mprj_io_0 == 8'h05);
-		wait(mprj_io_0 == 8'h06);
-		wait(mprj_io_0 == 8'h07);
-		wait(mprj_io_0 == 8'h08);
-		wait(mprj_io_0 == 8'h09);
-		wait(mprj_io_0 == 8'h0A);
-		wait(mprj_io_0 == 8'hFF);
-		wait(mprj_io_0 == 8'h00);
-
-		`ifdef GL
-	    	$display("Monitor: Test 1 Mega-Project IO (GL) Passed");
-		`else
-		    $display("Monitor: Test 1 Mega-Project IO (RTL) Passed");
-		`endif
-	    $finish;
-	end
+   // *********************************************************************************
+   // reset
 
 	initial begin
+
 		RSTB <= 1'b0;
 		CSB  <= 1'b1;		// Force CSB high
+
 		#2000;
+      $display("Releasing reset.");
 		RSTB <= 1'b1;	    	// Release reset
+
 		#3_00_000;
 		CSB = 1'b0;		// CSB can be released
 	end
+
+   // *********************************************************************************
+   // power up
 
 	initial begin		// Power-up sequence
 		power1 <= 1'b0;
@@ -128,10 +135,59 @@ module toysram_scan_tb;
 		power4 <= 1'b1;
 	end
 
+   // *********************************************************************************
+   // monitors
+
 	always @(mprj_io) begin
-		#1 $display("MPRJ-IO state = %b ", mprj_io[8:0]);
+      if (debug) begin
+		   #1 $display("   MPRJ-IO TE    %b ", mprj_io[`PIN_TE]);
+		   #1 $display("   MPRJ-IO SCAN  %b ", {mprj_io[`PIN_SCAN_CLK], mprj_io[`PIN_SCAN_IN], mprj_io[`PIN_SCAN_OUT]});
+		   #1 $display("   MPRJ-IO RA0   %b ", {mprj_io[`PIN_RA0_CLK], mprj_io[`PIN_RA0_RST], mprj_io[`PIN_RA0_R0_EN], mprj_io[`PIN_RA0_R1_EN], mprj_io[`PIN_RA0_W0_EN]});
+      end
 	end
 
+   initial begin
+        wait(mprj_io[`PIN_TE] == 1'b1);
+        $display("Test Enable is active.");
+        wait(mprj_io[`PIN_TE] == 1'b0);
+        $display("Test Enable is disabled.  Test complete.");
+        #1000;
+        $finish;
+   end
+
+   // *********************************************************************************
+   // test
+
+   initial begin
+
+      pin_te <= 1'b0;
+      pin_scan_clk <= 1'b0;
+      pin_scan_in <= 1'b0;
+      pin_ra0_clk <= 1'b0;
+      pin_ra0_rst <= 1'b0;
+      pin_ra0_r0_en <= 1'b0;
+      pin_ra0_r1_en <= 1'b0;
+      pin_ra0_w0_en <= 1'b0;
+      wait(RSTB == 1'b1);
+
+      #500;
+      $display("Checking scanout.");
+      if (pin_scan_out !== 1'b0) begin
+   		$display("%c[1;31m",27);
+         $display("*ERROR* Scanout is not 0.");
+   		$display("%c[0m",27);
+	   	$finish;
+      end
+      $display("Setting Test Enable.");
+      pin_te <= 1'b1;
+
+      #1000;
+      pin_te <= 1'b0;
+
+   end
+
+   // *********************************************************************************
+   // hook stuff up
 	wire flash_csb;
 	wire flash_clk;
 	wire flash_io0;
